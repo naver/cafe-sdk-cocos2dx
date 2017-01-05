3bd80e53b4bf84408a39f57d7971bf81eba0174e
wonkuym.kim <naver@naverui-MacBook-Pro-51.local>
2017-01-05 13:28:41 +0900

    [NO-BTS] 샘플 디렉토리 경로 수정.

--- sample/cocos2d/cocos/platform/android/java/gen/R.java.d
+++ sample/cocos2d/cocos/platform/android/java/gen/R.java.d
@@ -1,1 +1,1 @@
- : /Users/naver/dev/dev-cocos2d/cocos2dx-public/sample/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml \
+ : /Users/naver/dev/plug-publics/cocos2dx-public/sample/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml \
--- sample/cocos2d/plugin/protocols/proj.android/gen/R.java.d
+++ sample/cocos2d/plugin/protocols/proj.android/gen/R.java.d
@@ -1,1 +1,1 @@
- : /Users/naver/dev/dev-cocos2d/cocos2dx-public/sample/cocos2d/plugin/protocols/proj.android/bin/AndroidManifest.xml \
+ : /Users/naver/dev/plug-publics/cocos2dx-public/sample/cocos2d/plugin/protocols/proj.android/bin/AndroidManifest.xml \
